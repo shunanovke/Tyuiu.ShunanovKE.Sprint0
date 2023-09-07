476af1adf4931fbe7bad460cde120b6ba5c6016f
shunanovke <shunan05@gmail.com>
2023-09-07 13:49:33 +0500

    Task2

---
+++ Tyuiu.ShunanovKE.Sprint0.Task2.V0.Lib/Tyuiu.ShunanovKE.Sprint0.Task2.V0.Lib.cpp
@@ -0,0 +1,15 @@
+﻿// Tyuiu.ShunanovKE.Sprint0.Task2.V0.Lib.cpp : Defines the functions for the static library.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "../../Tyuiu.Cours.cpp/Tyuiu.Cours.cpp.cpp"
+
+class Service :public ISprint0Task2V0
+{
+	virtual int Add(int a, int b) override
+	{
+		return a + b;
+	}
+
+};
