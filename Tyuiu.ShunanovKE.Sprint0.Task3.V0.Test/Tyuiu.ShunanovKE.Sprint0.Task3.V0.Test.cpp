d4c5f3925f43f84422f72b619085cbbde7f92aff
shunanovke <shunan05@gmail.com>
2023-09-10 19:49:10 +0500

    Task3

---
+++ Tyuiu.ShunanovKE.Sprint0.Task3.V0.Test/Tyuiu.ShunanovKE.Sprint0.Task3.V0.Test.cpp
@@ -0,0 +1,29 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Tyuiu.ShunanovKE.Sprint0.Task3.V0.Lib/Tyuiu.ShunanovKE.Sprint0.Task3.V0.Lib.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest2
+{
+	TEST_CLASS(UnitTest2)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			//Init
+			ISprint0Task3V0* date = new Service1();
+			int a = 3;
+			int b = 4;
+			int c = 5;
+			int d;
+
+			//run
+			d = date->Add(a, b, c);
+
+			//Valid
+			Assert::AreEqual(12,d)
+		}
+	};
+}
