9b0f68d75d7addb66e55a201635e43448b36ac53
shunanovke <shunan05@gmail.com>
2023-09-11 21:35:28 +0500

    Task4

---
+++ Tyuiu.ShunanovKE.Sprint0.Task4.V0.Lib/Tyuiu.ShunanovKE.Sprint0.Task4.V0.Lib.cpp
@@ -0,0 +1,16 @@
+﻿// Tyuiu.ShunanovKE.Sprint0.Task4.V0.Lib.cpp : Defines the functions for the static library.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "../../Tyuiu.Cours.cpp/Tyuiu.Cours.cpp.cpp"
+
+// TODO: This is an example of a library function
+class Service2 : public ISprint0Task4
+{
+	virtual int Calculate(int a, int b, int c, int d) override
+	{
+		return a - b / c * d;
+	}
+
+};
---
+++ Tyuiu.ShunanovKE.Sprint0.Task4.V0.Test/Tyuiu.ShunanovKE.Sprint0.Task4.V0.Test.cpp
@@ -0,0 +1,29 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Tyuiu.ShunanovKE.Sprint0.Task4.V0.Lib/Tyuiu.ShunanovKE.Sprint0.Task4.V0.Lib.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest3
+{
+	TEST_CLASS(UnitTest3)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{ //Init
+			ISprint0Task4* ans = new Service2();
+			int a = 25;
+			int b = 8;
+			int c = 4;
+			int d = 9;
+			int ans1;
+
+		  //run
+			ans1 = ans->Calculate(a, b, c, d);
+
+		  //Valid
+			Assert::AreEqual(7, ans1);
+		}
+	};
+}
