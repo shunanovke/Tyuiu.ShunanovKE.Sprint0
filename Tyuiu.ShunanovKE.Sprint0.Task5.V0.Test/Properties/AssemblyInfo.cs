1b29837185eb69b7247bfef3ce2ef31eadc7de43
shunanovke <shunan05@gmail.com>
2023-09-29 20:24:36 +0500

    Task5

---
+++ Tyuiu.ShunanovKE.Sprint0.Task5.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ShunanovKE.Sprint0.Task5.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("Tyuiu.ShunanovKE.Sprint0.Task5.V0.Test")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e5d0c4e5-c487-4991-85d9-ee4d02787184")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.ShunanovKE.Sprint0.Task5.V0/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.ShunanovKE.Sprint0.Task5.V0.Lib;
+
+namespace Tyuiu.ShunanovKE.Sprint0.Task5.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("A + B = " + DataService.Addition(12,45));
+            Console.WriteLine("A - B = " + DataService.Subtraction(45,12));
+            Console.WriteLine("A * B = " + DataService.Multiplication(10,13));
+            Console.WriteLine("A / B = " + DataService.Division(18,2));
+            Console.WriteLine("A / B = " + DataService.Division(18,0));
+            Console.ReadKey();
+        }
+    }
+}
